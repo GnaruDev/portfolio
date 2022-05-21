64959f08eac127e25191d0d261c1cd46a71af79d
GnaruDev <gmblake121@outlook.com>
2022-05-21 16:14:32 -0500

    Updated skills section to reflect new skills.

--- html/aboutme.php
+++ html/aboutme.php
@@ -1,83 +1,92 @@
 <!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>GB | About Me</title>
-		<meta name ="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="../styles/stylesheetaltpages.css">
-		<link rel="stylesheet" href="../styles/stylesheetnav.css">
-		<script src="https://kit.fontawesome.com/6a47b417eb.js" crossorigin="anonymous"></script>
-		<style>
-			@import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Work+Sans:wght@300&display=swap');
-		</style>
-	</head>
+<html lang="en">
 
-  	<body>
-		<div class = "topfixed">
-			<header>
-				<nav>
-					<ul class="nav-list">
-							<li class="nav-item"><a href="../index.php">Home</a></li>
-							<li class="nav-item"><a href="aboutme.php">About</a></li>
-							<li class="nav-item"><a href="mywork.php">My&nbsp;Work</a></li>
-							<li class="nav-item"><a href="contact.php">Contact</a></li>
-					</ul>
-				</nav>
-			</header>
+<head>
+	<meta charset="UTF-8">
+	<title>GB | About Me</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="../styles/stylesheetaltpages.css">
+	<link rel="stylesheet" href="../styles/stylesheetnav.css">
+	<script src="https://kit.fontawesome.com/6a47b417eb.js" crossorigin="anonymous"></script>
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Work+Sans:wght@300&display=swap');
+	</style>
+</head>
+
+<body>
+	<div class="topfixed">
+		<header>
+			<nav>
+				<ul class="nav-list">
+					<li class="nav-item"><a href="../index.php">Home</a></li>
+					<li class="nav-item"><a href="aboutme.php">About</a></li>
+					<li class="nav-item"><a href="mywork.php">My&nbsp;Work</a></li>
+					<li class="nav-item"><a href="contact.php">Contact</a></li>
+				</ul>
+			</nav>
+		</header>
+	</div>
+
+	<main>
+
+
+		<p><br /><br /><br /><br /></p>
+		<h1 class="altpgtitle">About</h1>
+		<div class="flexrow">
+			<div class="flexcolumn1">
+				<p class="paragraph">
+					My name is Gina Blake and I'm a passionate Texas based web developer. I have spent most of my career working in the software industry as a technical software support.
+					These positions have helped me become well versed in troubleshooting and have given me well rounded knowledge of different technologies over the years. I enjoy coding and creating eye catching, accessible, websites for all.
+				</p>
+			</div>
+
+			<div class="flexcolumn2">
+				<ul class="altpglist">
+					<li><span>Email:</span>&nbsp;<script>
+							document.write(atob("Z21ibGFrZTEyMUBvdXRsb29rLmNvbQ=="));
+						</script>
+					</li>
+				</ul>
+				<div class="gitdiv">
+					<a href="https://github.com/gnarudev" class="githublink" target="_blank"><i class="fab fa-github fa-3x"></i></a>
+				</div>
+			</div>
 		</div>
+		<p><br /><br /><br /><br /></p>
 
-		<main>
+		<h1 class="altpgtitle">Skills</h1>
+		<div class="flexrow">
+			<div class="scolumn1">
+				<ul class="skillslist">
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>JavaScript</li>
+					<li>Bootstrap</li>
+					<li>jQuery</li>
+					<li>SQL</li>
+					<li>PHP</li>
+				</ul>
+			</div>
 
+			<div class="scolumn2">
+				<ul class="skillslist">
+					<li>Node.js</li>
+					<li>REST APIs</li>
+					<li>Git collaboration</li>
+					<li>Adobe Creative Cloud</li>
+					<li>Responsive web design</li>
+					<li>Web accessibility</li>
+					<li>Cross-browser Compatibility</li>
+				</ul>
+			</div>
+		</div>
+	</main>
 
-			<p><br /><br /><br /><br /></p>
-			<h1 class = "altpgtitle">About</h1>
-				<div class="flexrow">
-			  		<div class="flexcolumn1">
-			    		<p class="paragraph"> 
-							My name is Gina Blake and I'm a passionate Texas based web developer. I have spent most of my career working in the software industry as a technical software support.
-							These positions have helped me become well versed in troubleshooting and have given me well rounded knowledge of different technologies over the years. I enjoy coding and creating eye catching, accessible, websites for all.
-						</p>
-			  		</div>
-
-			 		<div class="flexcolumn2">
-						<ul class="altpglist">
-							<li><span>Email:</span>&nbsp;<script>document.write(atob("Z21ibGFrZTEyMUBvdXRsb29rLmNvbQ=="));</script></li>
-					 	</ul>
-						 <div class="gitdiv">
-						 	<a href="https://github.com/gnarudev" class="githublink" target="_blank"><i class="fab fa-github fa-3x"></i></a>
-						</div>
-			  		</div>
-				</div>
-			<p><br /><br /><br /><br /></p>
-
-			<h1 class = "altpgtitle">Skills</h1>
-				<div class ="flexrow">
-					<div class="scolumn1">
-						<ul class="skillslist">
-							<li>HTML</li>
-							<li>CSS</li>
-							<li>JavaScript</li>
-							<li>SQL</li>
-							<li>PHP</li>
-						</ul>
-					</div>
-
-					<div class="scolumn2">
-						<ul class="skillslist">
-							<li>Git collaboration</li>
-							<li>Adobe Illustrator</li>
-							<li>Adobe Photoshop</li>
-							<li>Responsive web design</li>
-							<li>Cross-browser Compatibility</li>
-						</ul>
-					</div>
-				</div>
-		</main>
-	
-	    <div class = "footercontainer">
-			<footer>
+	<div class="footercontainer">
+		<footer>
 			© Gina Blake 2022
-			</footer>
-		</div>
-  </body>
+		</footer>
+	</div>
+</body>
+
 </html>
